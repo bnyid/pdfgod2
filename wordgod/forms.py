220539eca545f4041b8512f94a579f2459aa9c99
bnyeducation <bny@bnyeducation.com>
2024-05-20 14:25:36 +0900

    시험 멘트 추가

--- wordgod/forms.py
+++ wordgod/forms.py
@@ -1,9 +1,5 @@
 from django import forms
 
-class TextInputForm(forms.Form):
-    text = forms.CharField(widget=forms.Textarea, label='텍스트 입력')
-
-
 class ExcelUploadForm(forms.Form):
     file = forms.FileField(label='엑셀 파일 업로드')
     
--- wordgod/views.py
+++ wordgod/views.py
@@ -1,15 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .forms import ExcelUploadForm, TextInputForm
+from .forms import ExcelUploadForm
 from google.cloud import texttospeech
 from pydub import AudioSegment
 import pandas as pd
 import tempfile
-import os
+import os, io
 
 def wordgod_view(request):
     form = ExcelUploadForm()
     return render(request, 'wordgod.html', {'form': form})
+
+
+from google.cloud import texttospeech
+import io
+
+def ssml_text_to_speech(ssml_text, language_code, voice_name):
+    client = texttospeech.TextToSpeechClient()
+    input_text = texttospeech.SynthesisInput(ssml=ssml_text)
+    voice = texttospeech.VoiceSelectionParams(
+        language_code=language_code,
+        name=voice_name
+    )
+    audio_config = texttospeech.AudioConfig(
+        audio_encoding=texttospeech.AudioEncoding.MP3
+    )
+    response = client.synthesize_speech(
+        input=input_text, voice=voice, audio_config=audio_config
+    )
+    return AudioSegment.from_file(io.BytesIO(response.audio_content), format="mp3")
+
+
 
 def text_to_speech_with_google(text, language_code, voice_name, output_format="mp3"):
     client = texttospeech.TextToSpeechClient()
@@ -52,7 +73,7 @@ def for_study(request):
         if form.is_valid():
             excel_file = request.FILES['file']
             df = pd.read_excel(excel_file, header=None)
-            df.columns = ['Index', 'Word', 'Meaning']
+            df.columns = ['Word', 'Meaning']
             # 업로드된 엑셀 파일을 읽어 데이터프레임으로 변환하고, 열 이름을 지정합니다.
 
             combined_audio = AudioSegment.empty()
@@ -82,7 +103,7 @@ def for_study(request):
             output_path = tempfile.NamedTemporaryFile(delete=False, suffix=".mp3").name
             combined_audio.export(output_path, format="mp3")
             # 최종 결합된 오디오 시퀀스를 MP3 파일로 저장합니다.
-
+        
             with open(output_path, 'rb') as f:
                 response = HttpResponse(f.read(), content_type='audio/mpeg')
                 response['Content-Disposition'] = f'attachment; filename="output_combined.mp3"'
@@ -105,10 +126,32 @@ def for_exam(request):
         if form.is_valid():
             excel_file = request.FILES['file']
             df = pd.read_excel(excel_file, header=None)
-            df.columns = ['Index', 'Word', 'Meaning']
-            
-            combined_audio = AudioSegment.empty()
-            
+            df.columns = ['Word', 'Meaning']
+
+            combined_audio = AudioSegment.silent(duration=3000)  # 3초의 침묵
+
+            # SSML을 사용하여 로고 메시지를 부드럽게 읽도록 설정
+            logo_message_text = """
+            <speak>
+                <prosody rate="1.5" pitch="-2st">
+                    <say-as interpret-as="characters">B&Y</say-as>
+                </prosody>
+            </speak>
+            """
+
+            logo_message = ssml_text_to_speech(logo_message_text, language_code='en-US', voice_name='en-US-Wavenet-D')
+            combined_audio += logo_message
+
+            # 일반적인 목소리로 "English Word test" 추가
+            intro_message = text_to_speech_with_google("English Word test", language_code='en-US', voice_name='en-US-Wavenet-D')
+            combined_audio += intro_message
+            combined_audio += AudioSegment.silent(duration=3000)  # 3초의 침묵 추가
+
+            # 일반적인 목소리로 "시험을 시작합니다" 추가
+            start_message = text_to_speech_with_google("시험을 시작합니다", language_code='ko-KR', voice_name='ko-KR-Wavenet-A')
+            combined_audio += start_message
+            combined_audio += AudioSegment.silent(duration=3000)  # 3초의 침묵 추가
+
             for index, row in df.iterrows():
                 word = row['Word']
                 
@@ -118,14 +161,14 @@ def for_exam(request):
                     if i == 0:
                         combined_audio += AudioSegment.silent(duration=800)
 
-                combined_audio += AudioSegment.silent(duration=7000)
+                combined_audio += AudioSegment.silent(duration=9000)
             
             output_path = tempfile.NamedTemporaryFile(delete=False, suffix=".mp3").name
             combined_audio.export(output_path, format="mp3")
-            
+        
             with open(output_path, 'rb') as f:
                 response = HttpResponse(f.read(), content_type='audio/mpeg')
-                response['Content-Disposition'] = f'attachment; filename="exam_words.mp3"'
+                response['Content-Disposition'] = f'attachment; filename="output_test.mp3"'
                 os.remove(output_path)
                 
                 return response
