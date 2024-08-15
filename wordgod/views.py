77210b501e5b51cbf6b6600a1823ac8d9e140c84
James_Yun <apple@apples-MacBook-Pro.local>
2024-08-15 15:23:24 +0900

    엑셀 번호열 추가, 학습용 파일 한국말 제외

--- wordgod/views.py
+++ wordgod/views.py
@@ -4,9 +4,12 @@ from django.http import HttpResponse
 from .forms import ExcelUploadForm
 from google.cloud import texttospeech
 from pydub import AudioSegment
+from langdetect import detect
+from langdetect.lang_detect_exception import LangDetectException
+
 import pandas as pd
 import tempfile
-import os, io
+import os, io, re
 import urllib.parse
 import zipfile
 
@@ -75,40 +78,87 @@ def for_study(request):
         files = request.FILES.getlist('file')
         mp3_files = []
         for excel_file in files:
-            combined_audio = AudioSegment.silent(duration=1500)  # 1.5초의 침묵
+            df = pd.read_excel(excel_file, header=None)
+            df.columns = ['Number', 'Word', 'Meaning']  # 번호,영단어,우리말 뜻으로 컬럼 이름 지정
+
+            total_words_count = len(df)
+            combined_audio = AudioSegment.silent(duration=1500)
 
             logo_message_text = """
             <speak>
-                <prosody rate="1.5" pitch="-2st">
-                    <say-as interpret-as="characters">B&Y</say-as>
+                <prosody rate="x-high" pitch="low">
+                    B&Y
                 </prosody>
             </speak>
             """
+            
+            '''
+            x-low: 매우 낮음
+            low: 낮음
+            medium: 기본 음조 (기본값)
+            high: 높음
+            x-high: 매우 높음
+            '''
+            
+            
             logo_message = ssml_text_to_speech(logo_message_text, language_code='en-US', voice_name='en-US-Wavenet-D')
             combined_audio += logo_message
 
-            intro_message = text_to_speech_with_google("Word Practice", language_code='en-US', voice_name='en-US-Wavenet-D')
+            intro_message = text_to_speech_with_google("Word practice", language_code='en-US', voice_name='en-US-Wavenet-D')
             combined_audio += intro_message
-            combined_audio += AudioSegment.silent(duration=2500)
+            combined_audio += AudioSegment.silent(duration=500)
 
-            df = pd.read_excel(excel_file, header=None)
-            df.columns = ['Word', 'Meaning']
+            file_name = os.path.splitext(excel_file.name)[0]
+            match = re.search(r'\(([^)]+)\)', file_name)
+
+            if match:
+                file_title_text = match.group(1)
+            else:
+                file_title_text = ""  # 괄호 안에 내용이 없거나 괄호가 없는 경우 빈 문자열 할당
+
+            try:
+                intro_language = detect(file_title_text)
+                if intro_language == 'ko':
+                    title_language_code = 'ko-KR'
+                    title_voice_name = 'ko-KR-Wavenet-A'
+                else:
+                    title_language_code = 'en-US'
+                    title_voice_name = 'en-US-Wavenet-D'
+            except LangDetectException:
+                # 감지 실패 시 기본값 설정 (영어)
+                title_language_code = 'en-US'
+                title_voice_name = 'en-US-Wavenet-D'
+            
+            
+            if file_title_text:  # 빈 문자열이 아닌 경우에만 음성 합성 호출
+                title_message = text_to_speech_with_google(file_title_text, language_code=title_language_code, voice_name=title_voice_name)
+                combined_audio += title_message
+                combined_audio += AudioSegment.silent(duration=500)
+
+            dingdong_path = os.path.join(settings.BASE_DIR, 'static', 'sound', 'dingdong.mp3')
+            dingdong_sound = AudioSegment.from_file(dingdong_path, format="mp3")
+            combined_audio += dingdong_sound
+            combined_audio += AudioSegment.silent(duration=1000)
 
             for index, row in df.iterrows():
+                word_number = row['Number']  # 1열의 단어 번호
                 word = row['Word']
-                meaning = row['Meaning']
+                question_number_audio = text_to_speech_with_google(f"{word_number}번", language_code='ko-KR', voice_name='ko-KR-Wavenet-A')
+                combined_audio += question_number_audio
+                combined_audio += AudioSegment.silent(duration=1000)
                 for i in range(2):
                     audio_segment = text_to_speech_with_google(word, language_code='en-US', voice_name='en-US-Wavenet-D')
                     combined_audio += audio_segment
-                    if i == 0:
-                        combined_audio += AudioSegment.silent(duration=800)
-                combined_audio += AudioSegment.silent(duration=800)
-                audio_segment = text_to_speech_with_google(meaning, language_code='ko-KR', voice_name='ko-KR-Wavenet-A')
-                combined_audio += audio_segment
-                combined_audio += AudioSegment.silent(duration=800)
+                    combined_audio += AudioSegment.silent(duration=900)
+                    #if i == 0:
+                     #   combined_audio += AudioSegment.silent(duration=500)
+                
+                combined_audio += AudioSegment.silent(duration=1000)
+            combined_audio += dingdong_sound
+            combined_audio += AudioSegment.silent(duration=1500)
 
             base_filename = os.path.splitext(excel_file.name)[0]
-            output_path = os.path.join(tempfile.gettempdir(), f"{base_filename}_학습용.mp3")
+            output_path = os.path.join(tempfile.gettempdir(), f"{base_filename}_시험용.mp3")
             combined_audio.export(output_path, format="mp3")
             mp3_files.append(output_path)
 
@@ -120,7 +170,7 @@ def for_study(request):
 
         with open(zip_path, 'rb') as f:
             response = HttpResponse(f.read(), content_type='application/zip')
-            response['Content-Disposition'] = 'attachment; filename="output_학습용.zip"'
+            response['Content-Disposition'] = 'attachment; filename="output_시험용.zip"'
             os.remove(zip_path)
             return response
     return render(request, 'wordgod.html')
@@ -132,7 +182,8 @@ def for_exam(request):
         mp3_files = []
         for excel_file in files:
             df = pd.read_excel(excel_file, header=None)
-            df.columns = ['Word', 'Meaning']
+            df.columns = ['Number', 'Word', 'Meaning']  # 번호,영단어,우리말 뜻으로 컬럼 이름 지정
+
             total_words_count = len(df)
             combined_audio = AudioSegment.silent(duration=1500)
 
@@ -189,7 +240,7 @@ def for_exam(request):
 
         with open(zip_path, 'rb') as f:
             response = HttpResponse(f.read(), content_type='application/zip')
-            response['Content-Disposition'] = 'attachment; filename="output_시험용.zip"'
+            response['Content-Disposition'] = 'attachment; filename="output_학습용.zip"'
             os.remove(zip_path)
             return response
     return render(request, 'wordgod.html')
